f9f20fa218307511c72609e7680d4e77cb244083
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-03-04 12:46:59 -0500

    commit tree ca7db279164cc846d18c81c314176caa0f2c7f92

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  get '/' do
+ 
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -14,6 +20,36 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  get '/reviews' do
+    review=Review.all.to_json
+  end
+
+  delete '/reviews/:id' do
+    review=Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    # binding.pry
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review=Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
